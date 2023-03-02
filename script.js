9a00a8531735bf12b94c55d1985203299ec465c4
jasoncorreia87 <jasoncorreia87@gmail.com>
2023-03-02 15:23:08 -0300

    da trabalho

--- script.js
+++ script.js
@@ -59,3 +59,28 @@ nextBtn.addEventListener("click", function() {
 });
 
 
+const calcularBtn = document.getElementById('calcular');
+calcularBtn.addEventListener('click', calcularIMC);
+
+function calcularIMC() {
+  const peso = document.getElementById('peso').value;
+  const altura = document.getElementById('altura').value;
+  
+  if (peso === '' || altura === '') {
+    alert('Por favor, preencha todos os campos');
+    return;
+  }
+  
+  const imc = peso / (altura * altura);
+  const resultado = document.getElementById('resultado');
+  
+  if (imc < 18.5) {
+    resultado.innerHTML = `Seu IMC é ${imc.toFixed(2)}. Isso indica que você está abaixo do peso.`;
+  } else if (imc >= 18.5 && imc <= 24.9) {
+    resultado.innerHTML = `Seu IMC é ${imc.toFixed(2)}. Isso indica que você está com um peso saudável.`;
+  } else if (imc >= 25 && imc <= 29.9) {
+    resultado.innerHTML = `Seu IMC é ${imc.toFixed(2)}. Isso indica que você está com sobrepeso.`;
+  } else {
+    resultado.innerHTML = `Seu IMC é ${imc.toFixed(2)}. Isso indica que você está com obesidade.`;
+  }
+}
